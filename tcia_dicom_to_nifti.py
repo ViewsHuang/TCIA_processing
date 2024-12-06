92a90b6153493f1105f72657dd83ec9869a6fa40
Huan Huang <viewshuang@163.com>
2024-12-06 22:38:35 +0800

    Add MIP image generating code Selecting Lymphoma data from TCIA dataset Adding clinical data

--- tcia_dicom_to_nifti.py
+++ tcia_dicom_to_nifti.py
@@ -29,10 +29,10 @@ def find_studies(path_to_data):
 
     for dir in patient_dirs:
         sub_dirs = list(dir.glob('*'))
-        #print(sub_dirs)
+        # print(sub_dirs)
         study_dirs.extend(sub_dirs)
-        
-        #dicom_dirs = dicom_dirs.append(dir.glob('*'))
+
+        # dicom_dirs = dicom_dirs.append(dir.glob('*'))
     return study_dirs
 
 
@@ -46,25 +46,25 @@ def identify_modalities(study_dir):
     for dir in sub_dirs:
         first_file = next(dir.glob('*.dcm'))
         ds = pydicom.dcmread(str(first_file))
-        #print(ds)
+        # print(ds)
         modality = ds.Modality
         modalities[modality] = dir
-    
+
     modalities["ID"] = ds.StudyInstanceUID
     return modalities
 
 
 def dcm2nii_CT(CT_dcm_path, nii_out_path):
     # conversion of CT DICOM (in the CT_dcm_path) to nifti and save in nii_out_path
-    with tempfile.TemporaryDirectory() as tmp: #convert CT
+    with tempfile.TemporaryDirectory() as tmp:  # convert CT
         tmp = plb.Path(str(tmp))
         # convert dicom directory to nifti
         # (store results in temp directory)
-        dicom2nifti.convert_directory(CT_dcm_path, str(tmp), 
+        dicom2nifti.convert_directory(CT_dcm_path, str(tmp),
                                       compression=True, reorient=True)
         nii = next(tmp.glob('*nii.gz'))
         # copy niftis to output folder with consistent naming
-        shutil.copy(nii, nii_out_path/'CT.nii.gz')
+        shutil.copy(nii, nii_out_path / 'CT.nii.gz')
 
 
 def dcm2nii_PET(PET_dcm_path, nii_out_path):
@@ -72,19 +72,19 @@ def dcm2nii_PET(PET_dcm_path, nii_out_path):
     first_pt_dcm = next(PET_dcm_path.glob('*.dcm'))
     suv_corr_factor = calculate_suv_factor(first_pt_dcm)
 
-    with tempfile.TemporaryDirectory() as tmp: #convert PET
+    with tempfile.TemporaryDirectory() as tmp:  # convert PET
         tmp = plb.Path(str(tmp))
         # convert dicom directory to nifti
         # (store results in temp directory)
-        dicom2nifti.convert_directory(PET_dcm_path, str(tmp), 
-                                    compression=True, reorient=True)
+        dicom2nifti.convert_directory(PET_dcm_path, str(tmp),
+                                      compression=True, reorient=True)
         nii = next(tmp.glob('*nii.gz'))
         # copy nifti to output folder with consistent naming
-        shutil.copy(nii, nii_out_path/'PET.nii.gz')
+        shutil.copy(nii, nii_out_path / 'PET.nii.gz')
 
         # convert pet images to quantitative suv images and save nifti file
-        suv_pet_nii = convert_pet(nib.load(nii_out_path/'PET.nii.gz'), suv_factor=suv_corr_factor)
-        nib.save(suv_pet_nii, nii_out_path/'SUV.nii.gz')
+        suv_pet_nii = convert_pet(nib.load(nii_out_path / 'PET.nii.gz'), suv_factor=suv_corr_factor)
+        nib.save(suv_pet_nii, nii_out_path / 'SUV.nii.gz')
 
 
 def conv_time(time_str):
@@ -110,7 +110,7 @@ def convert_pet(pet, suv_factor):
     # function for conversion of PET values to SUV (should work on Siemens PET/CT)
     affine = pet.affine
     pet_data = pet.get_fdata()
-    pet_suv_data = (pet_data*suv_factor).astype(np.float32)
+    pet_suv_data = (pet_data * suv_factor).astype(np.float32)
     pet_suv = nib.Nifti1Image(pet_suv_data, affine)
     return pet_suv
 
@@ -120,28 +120,28 @@ def dcm2nii_mask(mask_dcm_path, nii_out_path):
     mask_dcm = list(mask_dcm_path.glob('*.dcm'))[0]
     mask = pydicom.read_file(str(mask_dcm))
     mask_array = mask.pixel_array
-    
+
     # get mask array to correct orientation (this procedure is dataset specific)
-    mask_array = np.transpose(mask_array,(2,1,0) )  
+    mask_array = np.transpose(mask_array, (2, 1, 0))
     mask_orientation = mask[0x5200, 0x9229][0].PlaneOrientationSequence[0].ImageOrientationPatient
     if mask_orientation[4] == 1:
-        mask_array = np.flip(mask_array, 1 )
-    
+        mask_array = np.flip(mask_array, 1)
+
     # get affine matrix from the corresponding pet             
-    pet = nib.load(str(nii_out_path/'PET.nii.gz'))
+    pet = nib.load(str(nii_out_path / 'PET.nii.gz'))
     pet_affine = pet.affine
-    
+
     # return mask as nifti object
     mask_out = nib.Nifti1Image(mask_array, pet_affine)
-    nib.save(mask_out, nii_out_path/'SEG.nii.gz')   
-    
+    nib.save(mask_out, nii_out_path / 'SEG.nii.gz')
+
 
 def resample_ct(nii_out_path):
     # resample CT to PET and mask resolution
-    ct   = nib.load(nii_out_path/'CT.nii.gz')
-    pet  = nib.load(nii_out_path/'PET.nii.gz')
+    ct = nib.load(nii_out_path / 'CT.nii.gz')
+    pet = nib.load(nii_out_path / 'PET.nii.gz')
     CTres = nilearn.image.resample_to_img(ct, pet, fill_value=-1024)
-    nib.save(CTres, nii_out_path/'CTres.nii.gz')
+    nib.save(CTres, nii_out_path / 'CTres.nii.gz')
 
 
 def tcia_to_nifti(tcia_path, nii_out_path, modality='CT'):
@@ -168,7 +168,7 @@ def tcia_to_nifti_study(study_path, nii_out_path):
     study_path = plb.Path(study_path)
     modalities = identify_modalities(study_path)
     nii_out_path = plb.Path(nii_out_root / study_path.parent.name)
-    nii_out_path = nii_out_path/study_path.name
+    nii_out_path = nii_out_path / study_path.name
     os.makedirs(nii_out_path, exist_ok=True)
 
     ct_dir = modalities["CT"]
@@ -183,16 +183,15 @@ def tcia_to_nifti_study(study_path, nii_out_path):
     resample_ct(nii_out_path)
 
 
-def convert_tcia_to_nifti(study_dirs,nii_out_root):
+def convert_tcia_to_nifti(study_dirs, nii_out_root):
     # batch conversion of all patients
     for study_dir in tqdm(study_dirs):
-        
         patient = study_dir.parent.name
         print("The following patient directory is being processed: ", patient)
 
         modalities = identify_modalities(study_dir)
-        nii_out_path = plb.Path(nii_out_root/study_dir.parent.name)
-        nii_out_path = nii_out_path/study_dir.name
+        nii_out_path = plb.Path(nii_out_root / study_dir.parent.name)
+        nii_out_path = nii_out_path / study_dir.name
         os.makedirs(nii_out_path, exist_ok=True)
 
         ct_dir = modalities["CT"]
@@ -208,8 +207,10 @@ def convert_tcia_to_nifti(study_dirs,nii_out_root):
 
 
 if __name__ == "__main__":
-    path_to_data = plb.Path(sys.argv[1])  # path to downloaded TCIA DICOM database, e.g. '.../FDG-PET-CT-Lesions/'
-    nii_out_root = plb.Path(sys.argv[2])  # path to the to be created NiFTI files, e.g. '...tcia_nifti/FDG-PET-CT-Lesions/')
+    dicom_path = r"C:\Users\views\PycharmProjects\TCIA_Lymphoma_data\DICOM"
+    out_path = r"C:\Users\views\PycharmProjects\TCIA_Lymphoma_data\NIFTI"
+    path_to_data = plb.Path(dicom_path)  # path to downloaded TCIA DICOM database, e.g. '.../FDG-PET-CT-Lesions/'
+    nii_out_root = plb.Path(out_path)  # path to the to be created NiFTI files, e.g. '...tcia_nifti/FDG-PET-CT-Lesions/')
 
     study_dirs = find_studies(path_to_data)
     convert_tcia_to_nifti(study_dirs, nii_out_root)
